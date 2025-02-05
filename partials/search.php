19f5a079f50594c120bdae3266ee0bd8597dd467
dreamtech022 <dreamtech022@gmail.com>
2025-02-05 16:20:21 +0600

    search-event-resolved

--- partials/search.php
+++ partials/search.php
@@ -9,13 +9,13 @@ if(isset($_GET['search'])){
     $events = $apps->multipuleDisplay($sql);
 }  
 ?>
-
 <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded box-shadow">
     <div class="lh-100">
         <h6 class="mb-0 text-white lh-100">Events</h6>
     </div>
 </div>
-
+<?php
+if(count($events)>0){?>
 <div class="my-3 p-3 bg-white rounded box-shadow"> 
     <div class="row">
         <?php 
@@ -49,3 +49,17 @@ if(isset($_GET['search'])){
         ?>
     </div>
 </div>
+
+<?php
+}
+else{
+    ?>
+    <div class="d-flex justify-content-center align-items-center flex-column text-center w-100 vh-100">
+        <div class="p-4 shadow-lg bg-white rounded">
+            <h1 class="display-4 text-danger">Oops! No More Event</h1> 
+            <a href="index.php" class="btn btn-success px-4 py-2 mt-3 rounded-pill">Go to Home</a>
+        </div>
+    </div>
+    <?php
+}
+?>
